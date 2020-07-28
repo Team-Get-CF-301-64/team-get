b9d7184eab764fd4340043f4fafea75bbed8f23d
bromero <blakeromero001@gmail.com>
2020-07-27 17:28:32 -0700

    working on activity api

--- server.js
+++ server.js
@@ -75,8 +75,39 @@ function renderHome(request, response) {
 function renderResults(request, response) {
 
   try{
+    // let searchCity = request.body.search[0];
+    // let searchCategory = request.body.search[1];
+    let searchCategory = 'water';
+    let searchParams = '';
+    let url = 'http://api.opentripmap.com/0.1/en/places/radius';
+    // let url = 'http://api.opentripmap.com/0.1/en/places/radius?apikey=5ae2e3f221c38a28845f05b6c6943bdedcf9db68437c8a07ae749e05&radius=6000&lat=47.608013&lon=-122.335167&kinds=museums,water,nature_reserves,monuments_and_memorials';
 
-    response.status(200).send('/results');
+    // from search form to add parameters
+    if(searchCategory === 'museums'){searchParams += ',museums'};
+    if(searchCategory === 'water'){searchParams += ',water'};
+    if(searchCategory === 'nature'){searchParams += ',nature_reserves'};
+    if(searchCategory === 'monuments'){searchParams += ',monuments_and_memorials'};
+
+    let queryParams = {
+      apikey: process.env.TRAVEL_API_KEY,
+      // lat: request.query.latitude,
+      lat: 47.603649,
+      // lon: request.query.longitude,
+      lon: -122.330193,
+      // radius: request.query.radius,
+      radius: 1000,
+      kinds: searchParams
+    }
+
+    superagent.get(url)
+    .query(queryParams)
+    .then(results => {
+      let activitySearchResults = results.body;
+      let obj = activitySearchResults.features.map(activityObj => {
+        return new Activity(activityObj);
+      })
+      response.status(200).send('views/searches.ejs', {searchResults: obj});
+    })
   } catch(error){
     console.log('ERROR', error);
     response.status(500).send('Sorry, something went terribly wrong');
@@ -113,6 +144,13 @@ function Trip(){
 //   response.status(500).send('Sorry, something went terribly wrong');
 // });
 
+function Activity(obj){
+  this.name = obj.properties.name;
+  this.longitude = obj.geometry.coordinates[0];
+  this.latitude = obj.geometry.coordinates[1];
+  this.kinds = obj.properties.kinds;
+  this.rate = obj.properties.rate;
+}
 //==============================Errors=================================
 
 app.listen(PORT, () => {
